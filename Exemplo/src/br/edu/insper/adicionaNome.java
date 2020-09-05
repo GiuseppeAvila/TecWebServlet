953e4b2b23ad535228c0488a28cd4ef2f9488240
AntonioFuziy <antoniovf@al.insper.edu.br>
2020-09-05 17:52:12 -0400

    terminando primeira entraga

--- Exemplo/src/br/edu/insper/adicionaNome.java
+++ Exemplo/src/br/edu/insper/adicionaNome.java
@@ -29,7 +29,26 @@ public class adicionaNome extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String curso = request.getParameter("curso");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Seu perfil:</h1>");
+		out.println("<br>");
+		out.println("Nome: " + nome );
+		out.println("<br>");
+		out.println("Email: " + email );
+		out.println("<br>");
+		out.println("Curso: " + curso );
+		out.println("<br>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 	/**
@@ -39,13 +58,4 @@ public class adicionaNome extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String nome = request.getParameter("nome");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Nome: "+ nome );
-		out.println("</body>");
-		out.println("</html>");
-		}
 }
